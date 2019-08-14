bc96a4640de74e11f7eb5e8f422bb375e326ea81
weissab <andrea.weiss7@gmail.com>
2019-08-14 15:01:22 -0400

    trial segmenting p

--- Alignment_algorithms/k-mer_index.py
+++ Alignment_algorithms/k-mer_index.py
@@ -1,5 +1,5 @@
 #!/c/Users/Andrea/Anaconda3/python
-#Input arguments: filename to get text from, pattern, k - length of the k-mer to build index library from, m - number of mismatches
+#Input arguments: filename to get text from, pattern, k - length of the k-mer to build index library from, mismatch - number of mismatches allowed
 
 
 import sys
@@ -10,9 +10,9 @@ from libs.utils import read_genome
 def parse_args(argv):
     filename = argv[1]
     pattern = argv[2]
-    kmer_len = int(argv[3])
+    k = int(argv[3]) 
     mismatch = int(argv[4])
-    return filename, pattern, k
+    return filename, pattern, k, mismatch
 
 
 """kmer_index.py: A k-mer index for indexing a text."""
@@ -46,49 +46,65 @@ class Index(object):
         return hits
 
 
-def query_index(p, t, index): # index created from text t
+def query_index(p, t, index, mismatch): # index created from text t
     k = index.k
-    offsets = []
-    allmatches = set()
-    for i in index.query(p): #gives back the location where the first k bases match, still have to double check the rest of p to see if it matches t
-        #verification once there is a hit
-        for m in matches:
-            start = i + k
-            end = i + len(p)
-            if m < start or m - start + len(p) > len(t):
-                continue #skips rest of loop
-            
-            mismatches_found = 0
-            for j in range(0, start):
-                if not p[j] == t[m-start + j]:
-                    mismatches_found += 1
-                    if mismatches_found > mismatch:
+    offsets = set()
+    segment_length = int(round(len(p) / float(mismatch + 1)))
+    for i in range(mismatch + 1):
+        start = i * segment_length
+        end = min((i+1) * segment_length, len(p))
+        hits = index.query(p[start:end])
+        for h in hits: #gives back the location where the first k bases match, still have to double check the rest of p to see if it matches t
+            m = 0
+            match = True
+            for j in range(len(p) - k):
+                if p[j + k] != t[h + j + k]:
+                    m += 1
+                    if m > mismatch:
+                        match = False
                         break
-            for j in range (end, len(p)):
-                if not p[j] == t[m - start + j]:
-                    mismatches_found += 1
-                    if mismatches_found > mismatch:
-                        break
-
-            if mismatches_found <= mismatch:
-                all_matches.add(n - start)
-    return list(all_matches) #stored as a set so that there will not be multiple entries for matching multiple matching partitions
-
-
-
-
-
-
-     """    if p[k:] == t[i + k : i + len(p)]:
-            offsets.append(i)
-    return offsets """
+                if match:
+                    offsets.add(h)
+    return offsets, hits
 
 
 def main():
-    filename, pattern, kmer_len, mismatch = parse_args(sys.argv)
+    filename, pattern, k, mismatch = parse_args(sys.argv)
     t = read_genome(filename)
-    index = Index(t, kmer_len)
-    print(query_index(pattern, t, index))
+    index = Index(t, k)
+    exact_matches, hits = query_index(pattern, t, index, mismatch)
+    print("there are %d exact macthes and %d index hits, %s, %s" %(len(exact_matches), len(hits), exact_matches, hits))
 
 if __name__ == '__main__':
     main()
+
+
+
+""" 
+    def query_index(p, t, index): # index created from text t
+        k = index.k
+        matches = index.query(p)
+        allmatches = set()
+        for m in matches: #gives back the location where the first k bases match, still have to double check the rest of p to see if it matches t
+            #verification once there is a hit
+            for m in matches:
+                if m < start or m - start + len(p) > len(t):
+                    continue #skips rest of loop
+                
+                mismatch = 0
+                for j in range(0, start):
+                    if not p[j] == t[m-start + j]:
+                        if mismatch += 1
+                        if mismatches >n:
+                            break
+                for j in range (end, len(p)):
+                    if not p[j] == t[m - start + j]:
+                        mismatch += 1
+                        if mismatch > n:
+                            break
+                if mismatch <= n:
+                    all_matches.append(n - start)
+        return list(all_matches) #stored as a set so that there will not be multiple entries for matching multiple matching partitions
+    
+       
+        """
